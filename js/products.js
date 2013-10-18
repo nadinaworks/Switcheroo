dfa7affd214e1f053c08cf183abd226b128efcc4
Andrei Lupu <andreilupu>
2013-10-18 11:23:49 +0300

    pixelgrade config

--- js/products.js
+++ js/products.js
@@ -5,109 +5,65 @@ var $products,
 
 $products = {
 
-	visia : {
-		name     : 'Visia',
+	lens : {
+		name     : 'Lens',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/hgp2v0H.jpg',
-		url      : 'http://visia.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/visia-responsive-one-page-retina-wordpress-theme/5602067?ref=OriginalEXE',
-		tooltip  : 'Optional Tooltip'
+		img      : 'http://0.s3.envato.com/files/68068809/00_Teaser.__large_preview.png',
+		url      : 'http://pixelgrade.com/demos/lens/',
+		purchase : 'http://themeforest.net/item/lens-an-enjoyable-photography-wordpress-theme/5713452?WT.ac=follow&WT.seg_1=follow&WT.z_author=pixelgrade',
+		tooltip  : 'A pure yet intriguing visual experience for photographers'
 	},
 
-	bigwig_wp : {
-		name     : 'BigWig',
+	cityhub : {
+		name     : 'Cityhub',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/uoreaON.jpg',
-		url      : 'http://bigwig.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bigwig-modern-corporate-retina-wordpress-theme/5217458?ref=OriginalEXE'
+		img      : 'http://0.s3.envato.com/files/64607521/01_Teaser-Image-CityHub.__large_preview.png',
+		url      : 'http://pixelgrade.com/demos/cityhub/',
+		purchase : 'http://themeforest.net/item/cityhub-responsive-agency-blog-wordpress-theme/5425258?WT.ac=portfolio_item&WT.seg_1=portfolio_item&WT.z_author=pixelgrade',
+		tooltip  : 'A carefully crafted profesional theme for creatives agencies and freelancers'
 	},
 
-	surreal : {
-		name     : 'Surreal',
+	fuse : {
+		name     : 'Fuse',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/yISKP2n.jpg',
-		url      : 'http://surreal.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/surreal-one-page-parallax-wordpress-theme/5175040?ref=OriginalEXE'
+		img      : 'http://2.s3.envato.com/files/66230891/01_fuse-featured-image.__large_preview.png',
+		url      : 'http://pixelgrade.com/demos/fuse/',
+		purchase : 'http://themeforest.net/item/fuse-responsive-portfolio-blog-wordpress-theme/5136837?WT.ac=portfolio_item&WT.seg_1=portfolio_item&WT.z_author=pixelgrade',
+		tooltip  : 'A carefully crafted portfolio theme for creatives'
 	},
 
-	halfcreative : {
-		name     : 'Halfcreative',
+	senna : {
+		name     : 'Senna',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/roUmrA5.jpg',
-		url      : 'http://halfcreative.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/halfcreative-one-page-portfolio-wordpress-theme/4566909?ref=OriginalEXE'
+		img      : 'http://3.s3.envato.com/files/59882936/01_Teaser.__large_preview.png',
+		url      : 'http://pixelgrade.com/demos/senna/',
+		purchase : 'http://themeforest.net/item/senna-responsive-portfolio-blog-wordpress-theme/4609270?WT.ac=portfolio_item&WT.seg_1=portfolio_item&WT.z_author=pixelgrade',
+		tooltip  : 'The wordpress theme that all creatives have been waiting for'
 	},
 
-	bigwig_html : {
-		name     : 'BigWig',
+	bliv : {
+		name     : 'Bliv',
+		tag      : 'WP',
+		img      : 'http://3.s3.envato.com/files/49876560/01_Teaser.__large_preview.png',
+		url      : 'http://pixelgrade.com/demos/bliv/',
+		purchase : 'http://themeforest.net/item/bliv-responsive-minimal-wordpress-theme/4141443?WT.ac=portfolio_item&WT.seg_1=portfolio_item&WT.z_author=pixelgrade',
+		tooltip  : 'A Responsive theme in Swiss style'
+	},
+
+	start : {
+		name     : 'Start',
 		tag      : 'HTML',
-		img      : 'http://i.imgur.com/rOk3oel.jpg',
-		url      : 'http://bigwig.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bigwig-modern-corporate-html5-template/4801306?ref=OriginalEXE'
+		img      : 'http://3.s3.envato.com/files/40901809/01_Teaser.__large_preview.png',
+		url      : 'http://pixelgrade.com/demos/start/',
+		purchase : 'http://themeforest.net/item/start-metro-ui-responsive-admin-template/3408505?WT.ac=portfolio_item&WT.seg_1=portfolio_item&WT.z_author=pixelgrade',
+		tooltip  : 'Starting point for your next web app'
 	},
 
-	hydra_wp : {
-		name     : 'Hydra',
-		tag      : 'WP',
-		img      : 'http://i.imgur.com/RR11Nw9.jpg',
-		url      : 'http://hydra.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-wordpress-theme/4352507?ref=OriginalEXE'
-	},
-
-	contour : {
-		name     : 'Contour',
+	bird : {
+		name     : 'Bird',
 		tag      : 'HTML',
-		img      : 'http://i.imgur.com/5RntBHM.jpg',
-		url      : 'http://contour.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/contour-minimal-portfolio-corporate-template/4750249?ref=OriginalEXE'
-	},
-
-	brandspace : {
-		name     : 'Brandspace',
-		tag      : 'WP',
-		img      : 'http://i.imgur.com/9o0TRje.jpg',
-		url      : 'http://brandspace.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/brandspace-responsive-portfolio-business-theme/3502985?ref=OriginalEXE'
-	},
-
-	bon_atelier : {
-		name     : 'Bon Atelier',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/9o0TRje.jpg',
-		url      : 'http://brandspace.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bon-atelier-responsive-one-page-html5-template/3993470?ref=OriginalEXE'
-	},
-
-	mentor_html : {
-		name     : 'Mentor',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/f0g6JU5.jpg',
-		url      : 'http://mentor.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/mentor-premium-responsive-business-html5-template/2374421?ref=OriginalEXE'
-	},
-
-	mentor_wp : {
-		name     : 'Mentor',
-		tag      : 'WP',
-		img      : 'http://i.imgur.com/8CSwoGS.jpg',
-		url      : 'http://mentor.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/mentor-premium-responsive-html5-wordpress-theme/2560147?ref=OriginalEXE'
-	},
-
-	hydra_html : {
-		name     : 'Hydra',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/qIvg1HU.jpg',
-		url      : 'http://hydra.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-html5-template/4158593?ref=OriginalEXE'
-	},
-
-	neo_wp : {
-		name     : 'Neo',
-		tag      : 'WP',
-		img      : 'http://i.imgur.com/BXbOoUp.jpg',
-		url      : 'http://neo.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/neo-premium-responsive-business-wordpress-theme/2916870?ref=OriginalEXE'
+		img      : 'http://0.s3.envato.com/files/38965480/01_Teaser.__large_preview.png',
+		url      : 'http://pixelgrade.com/demos/bird/index2.html',
+		purchase : 'http://themeforest.net/item/bird-responsive-web-app-admin-template/3249981?WT.ac=portfolio_item&WT.seg_1=portfolio_item&WT.z_author=pixelgrade'
 	}
-
 };
